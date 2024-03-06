5eeb7dbd2aeb5e3743bc5bd0902b14497a5cb13e
RIG360 <office@rimpfl.at>
2024-03-06 14:09:06 +0100

    upload

---
+++ data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-wz-r-1",
+      "name": "WZ-R (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.064521371208908,
+        "pitch": -0.000939542413597394,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1118902041605168,
+          "pitch": 0.7314575817888951,
+          "rotation": 0,
+          "target": "1-wz-r-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-wz-r-2",
+      "name": "WZ-R (2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": -3.1336323379314397,
+        "pitch": -0.00020785866070305303,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7309953884610199,
+          "pitch": 0.957045637432909,
+          "rotation": 0,
+          "target": "2-wz-r-3"
+        },
+        {
+          "yaw": 1.6690820729541462,
+          "pitch": 0.5590526267506881,
+          "rotation": 0,
+          "target": "3-wz-r-4"
+        },
+        {
+          "yaw": 0.07559123906533038,
+          "pitch": 0.7801422325049039,
+          "rotation": 0,
+          "target": "0-wz-r-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-wz-r-3",
+      "name": "WZ-R (3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.5689464253412044,
+        "pitch": 0.0271292871924107,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7032755314273773,
+          "pitch": 0.9765649234722336,
+          "rotation": 0,
+          "target": "3-wz-r-4"
+        },
+        {
+          "yaw": -1.6154030164692834,
+          "pitch": 0.814540256389801,
+          "rotation": 0,
+          "target": "1-wz-r-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wz-r-4",
+      "name": "WZ-R (4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1632,
+      "initialViewParameters": {
+        "yaw": 1.5948683317606616,
+        "pitch": 0.03436191292478519,
+        "fov": 1.4408666390078466
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5912294115776966,
+          "pitch": 0.5381409196089919,
+          "rotation": 0,
+          "target": "1-wz-r-2"
+        },
+        {
+          "yaw": -1.5910584032568718,
+          "pitch": 0.8761583302905649,
+          "rotation": 0,
+          "target": "2-wz-r-3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mill34, WZ-R_240305",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
